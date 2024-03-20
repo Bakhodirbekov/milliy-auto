c06827ff2197e7c014543b73e3c3e30a1111750d
Bakhodirbekov <otabekotabek1661@gmail.com>
2024-03-20 16:51:37 +0500

    first commit

---
+++ js/sendtelegram.js
@@ -0,0 +1,39 @@
+
+
+
+//bot token
+var telegram_bot_id = "7079925059:AAFf9njaR2aeyH-j4B1xLkVbyHi8BpdOBzg"; // token'ni o'rniga Siz yaratgan Bot tokenini yozing
+//chat id
+var chat_id = 584323689; // 1111'ni o'rniga habar borishi kerak bo'lgan joyni ID'sini yozing (Batafsil videoda)
+var u_name, email, message;
+var ready = function() {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Ismi: " + u_name + "\nEmail: " + email + "\nIzoh: " + message;
+};
+var sendtelegram = function() {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
+
